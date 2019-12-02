63bbbc0cbeb1d2f936e56bba5bf208484d045992
Josh <jharris4491@gmail.com>
2019-12-01 17:11:16 -0800

    revising structure again

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-	tmpestbotserver "tmpest-bot-server"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-	server, error := tmpestbotserver.NewTmpestBotServer()
-	if error != nil {
-		return
-	}
+	server := newTmpestBotServer()
+	server.start()
+}
 
-	server.Start()
+type tmpestBotServer struct {
+	server *http.Server
+}
+
+func newTmpestBotServer() *tmpestBotServer {
+	return &tmpestBotServer{
+		server: &http.Server{
+			Addr:    ":44910",
+			Handler: oauth2RedirectHandler{},
+		},
+	}
+}
+
+func (s *tmpestBotServer) start() {
+	s.server.ListenAndServe()
+}
+
+type oauth2RedirectHandler struct{}
+
+func (handler oauth2RedirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Recieved a request!")
 }
