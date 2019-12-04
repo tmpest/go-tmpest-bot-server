1442bf9a4b50e356f00667b7569cd4d1c178590d
Josh <jharris4491@gmail.com>
2019-12-03 20:39:16 -0800

    leveraging new library

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tmpest/tmpest-bot-server
 
 go 1.13
+
+require github.com/tmpest/discord-bot v0.0.0-20191204040439-b58309b36484
--- main.go
+++ main.go
@@ -1,42 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
+	"github.com/tmpest/discord-bot/pkg/server"
 )
 
 func main() {
-	server := newTmpestBotServer()
-	server.start()
-}
-
-type tmpestBotServer struct {
-	server *http.Server
-}
-
-func newTmpestBotServer() *tmpestBotServer {
-	return &tmpestBotServer{
-		server: &http.Server{
-			Addr:    fmt.Sprintf(":%+v", os.Getenv("PORT")),
-			Handler: oauth2RedirectHandler{},
-		},
-	}
-}
-
-func (s *tmpestBotServer) start() {
-	fmt.Println("Starting the Tmpest Bot Server")
-	defer fmt.Println("Exiting! Goodbye!")
-
-	s.server.ListenAndServe()
-}
-
-type oauth2RedirectHandler struct{}
-
-func (handler oauth2RedirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Request received")
-	fmt.Printf("Query Params:\n%+v\n", r.URL.Query())
-	// Take code and exchange for Tokens
-
-	// Write tokens to Redis
+	server.New().Start()
 }
